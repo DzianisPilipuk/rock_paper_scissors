0f0bc975d5668a96f99e1cef1897a786e5031bd0
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-19 12:48:39 +0100

    Add animations to text

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let computerScore = 0;
 let userScore = 0;
 let userChoice = "";
 let computerChoice = "";
-let comparisonResult
+let comparisonResult;
 
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
@@ -11,23 +11,41 @@ buttons.forEach(button => {
     })
 })
 
+function textAnimation(elem, text, delay, buttons_disable) {
+    document.getElementById(elem).innerHTML = "";
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            document.getElementById(elem).innerHTML += text.charAt(i);
+        }, delay + 50*i);
+        if (buttons_disable) {
+            disableButtons();
+            if (userScore < 5 && computerScore < 5) {
+                setTimeout(() => {
+                    enableButtons();
+                }, delay + 50*text.length);
+            }
+        }
+    }
+}
+function displayVariableInformation() {
+    textAnimation("computer-choice-displayer", computerChoice, 250);
+    textAnimation("round-winner-displayer", comparisonResult, 1000, true);
+    setTimeout(() => {
+        document.getElementById("user-score-displayer").innerHTML = userScore;
+        document.getElementById("computer-score-displayer").innerHTML = computerScore;
+    }, 1000)
+    if (userScore == 5 || computerScore == 5) {
+        textAnimation("game-winner-displayer", declareWinner(), 2000);
+        textAnimation("reload-notification", "Reload the page to try one more time", 3000);
+    }
+}
 function playRound(choice) {
+    document.getElementById("computer-choice-displayer").innerHTML = "";
+    document.getElementById("round-winner-displayer").innerHTML = "";
     userChoice = choice;
     computerChoice = getComputerChoice();
     comparisonResult = compareChoices();
     displayVariableInformation();
-}
-function displayVariableInformation() {
-    // document.getElementById("user-choice-displayer").innerHTML = "Your choice: " + userChoice;
-    document.getElementById("computer-choice-displayer").innerHTML = computerChoice;
-    document.getElementById("round-winner-displayer").innerHTML = comparisonResult;
-    document.getElementById("user-score-displayer").innerHTML = userScore;
-    document.getElementById("computer-score-displayer").innerHTML = computerScore;
-    if (userScore == 5 || computerScore == 5) {
-        document.getElementById("game-winner-displayer").innerHTML = declareWinner();
-        document.getElementById("reload-notification").innerHTML = "Reload the page to try one more time";
-        disableButtons();
-    }
 }
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors']
@@ -59,5 +77,10 @@ function declareWinner() {
 function disableButtons() {
     buttons.forEach(button => {
         button.disabled = true
+    })
+}
+function enableButtons() {
+    buttons.forEach(button => {
+        button.disabled = false
     })
 }
